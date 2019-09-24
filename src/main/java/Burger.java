2fe2ede3e7087646d3d97d530875ba8185c3bc5d
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-24 23:36:15 +0400

    Добавлено

---
+++ src/main/java/Burger.java
@@ -0,0 +1,13 @@
+public class Burger {
+    int id;
+    String name;
+    int weightG;
+    float calorieContentKkl;
+    float proteinsG;
+    float fatsG;
+    float carbohydrates;
+    String description;
+    int price;
+
+
+}
